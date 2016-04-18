7397f3be91a6bd409df6c8cf926b929da16b94e3
Gamal Mohamed <Gamal Mohamed>
2016-04-19 01:54:02 +0200

    Integrating LCD & KEYPAD

--- Source/Source.ino
+++ Source/Source.ino
@@ -1,17 +1,29 @@
-#include "lcd.h"
+#include"lcd.h"
+#include"keypad.h"
 
+unsigned char key;
 void setup() {
-  LCD_init(); /* initialize LCD */
-  LCD_displayStringRowColumn(0,2,"WELCOME!");
-  LCD_displayStringRowColumn(1,3,"SAFE HOME");
-  _delay_ms(300); /* wait four seconds */
-
-  LCD_clearScreen(); /* clear the LCD display */
-  LCD_displayString("Activate System?");
-  LCD_displayStringRowColumn(1,5,"Y/N");
-
+  LCD_init();
+  LCD_displayStringRowColumn(0,1,"Welcome to Safe");
+  LCD_displayStringRowColumn(1,3,"Home system!");
+  _delay_ms(2000);
+  
+  LCD_clearScreen();
+  LCD_displayStringRowColumn(0,0,"Enter password: ");
+  LCD_goToRowColumn(1,0);
 }
 
 void loop() {
-  
+  /* if any switch pressed for more than 500 ms it counts more than one press */  
+    key = KeyPad_getPressedKey(); /* get the pressed key number */
+    if((key <= 9) && (key >= 0))
+    {
+      LCD_intgerToString(key); /* display the pressed keypad switch */
+      _delay_ms(500); /* Press time */
+    }
+    else
+    {
+      LCD_displayCharacter(key); /* display the pressed keypad switch */ 
+      _delay_ms(500); /* Press time */
+    }
 }
